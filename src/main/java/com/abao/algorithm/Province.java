d2e7c93b23fa3d362641e2f441208d0e9b003317
zhoushibao <zhoushibao@qiniu.com>
2023-06-16 22:30:16 +0800

    省份数量-wf-df-并查集

--- src/main/java/com/abao/algorithm/Province.java
+++ src/main/java/com/abao/algorithm/Province.java
@@ -8,11 +8,11 @@ public class Province {
     public static void main(String[] args) {
         int[][]city1= new int[][]{{1,1,0},{1,1,0},{0,0,1}};
         int[][]city2= new int[][]{{1,0,0},{0,1,0},{0,0,1}};
-        int[][]city3= new int[][]{{1,1,0,0,0,0,0,0,0,0},{1,1,1,1,0,0,0,0,0,0},{0,2,1,0,0,0,0,0,0,0},{0,1,0,0,1,0,0,0,0,0},{0,0,0,1,1,0,0,0,0,0},
+        int[][]city3= new int[][]{{1,1,0,0,0,0,0,0,0,0},{1,1,1,1,0,0,0,0,0,0},{0,1,1,0,0,0,0,0,0,0},{0,1,0,0,1,0,0,0,0,0},{0,0,0,1,1,0,0,0,0,0},
                 {0,0,0,0,0,1,1,1,0,0},{0,0,0,0,0,1,1,0,0,0},{0,0,0,0,0,1,0,1,1,0},{0,0,0,0,0,0,0,1,1,0},{0,0,0,0,0,0,0,0,0,1}};
-//        System.out.println(wf(city1));
-//        System.out.println(wf(city2));
-        System.out.println(wf(city3));
+//        System.out.println(joinSearch(city1));
+//        System.out.println(joinSearch(city2));
+        System.out.println(joinSearch(city3));
     }
 
     public static int cityNum(int[][]city){
@@ -66,4 +66,54 @@ public class Province {
         return province;
     }
 
+    // 并查集
+    public static int joinSearch(int [][]city){
+        int province = 0;
+        int[] parents = new int[city.length];
+        int[] levels = new int[city.length];
+        boolean[] visited = new boolean[city.length];
+        for(int i = 0;i<city.length ; i++){
+            parents[i] = i;
+            levels[i] = 1;
+        }
+        for(int i = 0;i<city.length;i++){
+            for(int j = i + 1;j<city.length ;j++){
+                if (!visited[j] && city[i][j] == 1){
+                    visited[j] = true;
+                    join(i,j,levels,parents);
+                }
+            }
+        }
+        for(int i = 0;i<city.length;i++){
+            if (parents[i] == i){
+                province ++ ;
+            }
+        }
+        return province;
+    }
+
+    // 合并指的是将leval小的root指向level大的root
+    private static void join(int i,int j,int[] levels,int[]parents){
+        // 查  root，顺便缩短路径
+        int rootJ = search(j,parents);
+        int rootI = search(i,parents);
+        // 并  root，将相连城市并到同一个root下
+        if(levels[rootI] < levels[rootJ]){
+            parents[i] = rootJ;
+        }else{
+            parents[j] = rootI;
+        }
+        if (levels[rootI] == levels[rootJ]){
+            levels[j] ++;
+        }
+    }
+    // 根据i和j的level，将level比较小的树的root改成大的root
+    private static int search(int i,int[] parents ) {
+        if(i == parents[i]){
+            return i;
+        }
+        // 这里将路径进行缩短，直接将叶子节点指向根节点，减少递归
+        parents[i] =  search(parents[i],parents);
+        return parents[i];
+    }
 }
